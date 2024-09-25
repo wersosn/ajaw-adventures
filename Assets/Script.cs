b738281d655b6f1526ca3e5b4ca5e7aba6c0ee82
WeronikaSosnowska <urszula03.2003@o2.pl>
2024-09-25 15:05:43 +0200

    Changed script

--- Assets/Script.cs
+++ Assets/Script.cs
@@ -6,6 +6,7 @@ public class Script : MonoBehaviour
 {
     // Reference 
     public Rigidbody2D myRigidBody;
+    public float flapS; // Flap strength
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,9 @@ public class Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myRigidBody.velocity = Vector2.up * 10;   
+        if(Input.GetKeyDown(KeyCode.Space) == true || Input.GetMouseButtonDown(0)) // Check if spacebar has been pressed on this frame
+        {
+            myRigidBody.velocity = Vector2.up * flapS; // Move body up
+        }  
     }
 }
